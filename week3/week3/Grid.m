7e7c6387dbad8b2cbffbe2ca02e66250ad1e6cef
Jehyeok <hook3748@gmail.com>
2014-07-24 20:02:01 +0900

    랜덤 배치

--- week3/week3/Grid.m
+++ week3/week3/Grid.m
@@ -16,12 +16,37 @@
     self = [super self];
     if (self) {
         rows = [[NSMutableArray alloc] initWithCapacity: rowNum];
-        [rows insertObject:[NSMutableArray arrayWithObjects:@1,@2,@3,nil] atIndex:0];
-        [rows insertObject:[NSMutableArray arrayWithObjects:@4,@0,@6,nil] atIndex:1];
-        [rows insertObject:[NSMutableArray arrayWithObjects:@7,@8,@5,nil] atIndex:2];
+        [rows insertObject:[[NSMutableArray alloc] initWithCapacity:rowNum] atIndex:0];
+        [rows insertObject:[[NSMutableArray alloc] initWithCapacity:rowNum] atIndex:1];
+        [rows insertObject:[[NSMutableArray alloc] initWithCapacity:rowNum] atIndex:2];
+        NSMutableArray *row;
         
-        blankPos[0] = 1;
-        blankPos[1] = 1;
+        NSMutableSet *set = [[NSMutableSet alloc] init];
+        while ([set count] != rowNum * rowNum - 1) {
+            int r = (arc4random() % (rowNum * rowNum - 1)) + 1;
+            
+            if ([set containsObject:[NSNumber numberWithInt:r]] == NO) {
+                if ([set count] < 3) {
+                    row = [rows objectAtIndex:0];
+                    NSLog(@"%@", [NSNumber numberWithInt:r]);
+                    [row addObject:[NSNumber numberWithInt:r]];
+                    [set addObject:[NSNumber numberWithInt:r]];
+                } else if ([set count] < 6) {
+                    row = [rows objectAtIndex:1];
+                    [row addObject:[NSNumber numberWithInt:r]];
+                    [set addObject:[NSNumber numberWithInt:r]];
+                } else {
+                    row = [rows objectAtIndex:2];
+                    [row addObject:[NSNumber numberWithInt:r]];
+                    [set addObject:[NSNumber numberWithInt:r]];
+                }
+            }
+        }
+        row = [rows objectAtIndex:2];
+        [row addObject:@0];
+        
+        blankPos[0] = 2;
+        blankPos[1] = 2;
     }
     return self;
 }
@@ -39,8 +64,8 @@
         return;
     }
     
-    NSMutableArray *row = [[NSMutableArray alloc] init];
-    NSMutableArray *nextRow = [[NSMutableArray alloc] init];
+    NSMutableArray *row;
+    NSMutableArray *nextRow;
     
     row = [rows objectAtIndex:blankPos[0]];
     nextRow = [rows objectAtIndex:blankPos[0] + direction];
@@ -57,7 +82,7 @@
     if ((blankPos[1] == 0 && direction == -1) || (blankPos[1] == 2 && direction == 1)) {
         return;
     }
-    NSMutableArray *row = [[NSMutableArray alloc] init];
+    NSMutableArray *row;
     
     row = [rows objectAtIndex:blankPos[0]];
     
